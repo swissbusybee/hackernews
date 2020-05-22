c50712dd3f489f3b3bc16debf5b138328ccddf16
Sue Rocha <swissbusybee@gmail.com>
2020-05-22 14:18:17 +0200

    Added Search

--- src/App.js
+++ src/App.js
@@ -20,7 +20,32 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: "",
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotID = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotID);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     function formatName(user) {
       return user.firstName + ' ' + user.lastName
@@ -33,17 +58,32 @@ class App extends Component {
 
     const helloWorld = "Howdy Cowgirl! Welcome to the Road to learn React"
 
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
         <h2>{helloWorld} {user.firstName}</h2>
         <h2>{helloWorld} {formatName(user)}</h2>
         <ul>
-          {list.map(item =>
+          <form>
+            <input
+              type="text"
+              onChange={this.onSearchChange}
+            />
+          </form>
+          {list.filter(isSearched(searchTerm)).map(item =>
             <div key={item.objectID}>
               <li><a href={item.url}>{item.title}</a></li>
               <li>{item.num_comments}</li>
               <li>{item.points}</li>
               <li>{item.author}</li>
+              <li><button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+                </button>
+              </li>
             </div>
           )}
         </ul>
