4c7e61cd68c1302c7e6576ba33de2bde5d27b17e
Sue Rocha <swissbusybee@gmail.com>
2020-05-25 13:35:19 +0200

    HOC Loading

--- src/App.js
+++ src/App.js
@@ -148,14 +148,12 @@ class App extends Component {
           />
         }
         <div className="interactions">
-          {isLoading
-            ? <Loading />
-            : <Button
-              onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
-            >
-              More
-            </Button>
-          }
+          <ButtonWithLoading
+            isLoading={isLoading}
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            More
+          </ButtonWithLoading>
         </div>
       </div>
     );
@@ -259,6 +257,13 @@ Button.propTypes = {
 
 const Loading = () =>
   <div>Loading ...</div>
+
+const withLoading = (Component) => ({ isLoading, ...rest }) =>
+  isLoading
+    ? <Loading />
+    : <Component {...rest} />
+
+const ButtonWithLoading = withLoading(Button);
 
 export default App;
 
